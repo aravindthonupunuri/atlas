5d7380b653d662b0dd0bf911b2bbbf20202fe112
Abhishek Kotha <Abhishek.Kotha@target.com>
2020-11-16 23:50:17 -0600

    Adding item state to UpdateListNotifyEvent (#30)

--- src/main/kotlin/com/tgt/lists/atlas/api/domain/UpdateListManager.kt
+++ src/main/kotlin/com/tgt/lists/atlas/api/domain/UpdateListManager.kt
@@ -3,6 +3,7 @@ package com.tgt.lists.atlas.api.domain
 import com.tgt.lists.atlas.api.domain.model.entity.ListEntity
 import com.tgt.lists.atlas.api.persistence.cassandra.ListRepository
 import com.tgt.lists.atlas.api.transport.mapper.ListMapper
+import com.tgt.lists.atlas.api.util.LIST_STATE
 import com.tgt.lists.atlas.kafka.model.UpdateListNotifyEvent
 import mu.KotlinLogging
 import reactor.core.publisher.Mono
@@ -29,7 +30,16 @@ class UpdateListManager(
                 .zipWhen {
                     val userMetaDataTO = ListMapper.getUserMetaDataFromMetadataMap(it.metadata)
                     eventPublisher.publishEvent(UpdateListNotifyEvent.getEventType(),
-                            UpdateListNotifyEvent(it.guestId!!, it.id!!, it.type!!, it.title, userMetaDataTO?.userMetaData), it.guestId!!)
+                            UpdateListNotifyEvent(
+                                    guestId = it.guestId!!,
+                                    listId = it.id!!,
+                                    listType = it.type!!,
+                                    listTitle = it.title,
+                                    listState = if (it.state != null)
+                                        LIST_STATE.values().first { listState -> listState.value == it.state!! }
+                                    else LIST_STATE.INACTIVE,
+                                    userMetaData = userMetaDataTO?.userMetaData
+                            ), it.guestId!!)
                 }
                 .map { it.t1 }
     }
--- src/main/kotlin/com/tgt/lists/atlas/kafka/model/UpdateListNotifyEvent.kt
+++ src/main/kotlin/com/tgt/lists/atlas/kafka/model/UpdateListNotifyEvent.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.tgt.lists.atlas.api.util.EventType
+import com.tgt.lists.atlas.api.util.LIST_STATE
 import java.util.*
 
 data class UpdateListNotifyEvent(
@@ -18,6 +19,9 @@ data class UpdateListNotifyEvent(
 
     @JsonProperty("list_title")
     val listTitle: String?,
+
+    @JsonProperty("list_state")
+    val listState: LIST_STATE?,
 
     @JsonProperty("user_meta_data")
     val userMetaData: Map<String, Any>? = null,
