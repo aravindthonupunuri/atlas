fd285366458c2d6b21318338ff87b5fae0c062d7
Sravani BhumaKoppula <Sravani.BhumaKoppula@target.com>
2021-01-27 14:00:52 -0600

    do not republish to dlq after failure to publish an event (#68)
    * do not republish to dlq after failure to publish an event
    
    * update
    
    * update error message to include event type

--- src/main/kotlin/com/tgt/lists/atlas/api/domain/EventPublisher.kt
+++ src/main/kotlin/com/tgt/lists/atlas/api/domain/EventPublisher.kt
@@ -2,9 +2,8 @@ package com.tgt.lists.atlas.api.domain
 
 import com.tgt.lists.atlas.api.type.EventType
 import com.tgt.lists.atlas.api.util.TestListEvaluator
-import com.tgt.lists.msgbus.ListsDlqProducer
 import com.tgt.lists.msgbus.ListsMessageBusProducer
-import com.tgt.lists.msgbus.event.EventHeaderFactory
+import mu.KotlinLogging
 import org.apache.kafka.clients.producer.RecordMetadata
 import reactor.core.publisher.Mono
 import javax.inject.Inject
@@ -12,27 +11,13 @@ import javax.inject.Singleton
 
 @Singleton
 class EventPublisher(
-    @Inject private val listsMessageBusProducer: ListsMessageBusProducer<String, Any>,
-    @Inject private val listsDLQProducer: ListsDlqProducer<String, Any>,
-    @Inject private val eventHeaderFactory: EventHeaderFactory
+    @Inject private val listsMessageBusProducer: ListsMessageBusProducer<String, Any>
 ) {
+
+    private val logger = KotlinLogging.logger {}
 
     fun publishEvent(eventType: EventType, message: Any, partitionKey: String): Mono<RecordMetadata> {
         return listsMessageBusProducer.sendMessage(eventType, message, partitionKey, TestListEvaluator.evaluate())
-                .onErrorResume { handleMsgbusProducerError(eventType, message, partitionKey) }
-    }
-
-    private fun handleMsgbusProducerError(
-        eventType: EventType,
-        message: Any,
-        partitionKey: String
-    ): Mono<RecordMetadata> {
-        // Exception publishing item completion kafka event, so sending it to DLQ topic for retry
-        val headers = eventHeaderFactory.nextRetryHeaders(
-                eventHeaders = eventHeaderFactory.newEventHeaders(eventType = eventType, testMode = TestListEvaluator.evaluate()),
-                errorCode = 500,
-                errorMsg = "Failure publishing item completion kafka event with message with message $message " +
-                        "to message bus kafka topic, so publishing it to DLQ topic")
-        return listsDLQProducer.sendMessage(headers, message, partitionKey)
+                .doOnError { logger.error("Failed to publish kafka event for EventType $eventType with message $message to message bus topic kafka topic") }
     }
 }
